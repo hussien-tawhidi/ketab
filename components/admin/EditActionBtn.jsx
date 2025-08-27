2f3adc401d28dcaec571381f6e59fb20e1ea6cc5
hussien tawhidi <hussientawhidi710@gmail.com>
2025-08-27 20:44:11 +0330

    update books lists

---
+++ components/admin/EditActionBtn.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
+import { MdOutlineEdit } from "react-icons/md";
+
+export default function EditActionBtn({ onEdit, text }) {
+  const router = useRouter();
+  return (
+    <motion.button
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+      onClick={onEdit}
+      aria-label={`ویرایش ${text}`}
+      className='relative flex items-center justify-start w-[80px] h-[30px] px-[15px] text-[12px] font-medium text-light rounded-[10px] shadow-[5px_5px_0_rgb(0,0,0)] transition-all duration-300 active:translate-x-[3px] active:translate-y-[3px] active:shadow-darker group'>
+      <span className='transition-all duration-300 group-hover:text-transparent'>
+        {text}
+      </span>
+      <MdOutlineEdit className='absolute left-[10px] w-[12px] transition-all duration-300 group-hover:left-[43%]' />
+    </motion.button>
+  );
+}
